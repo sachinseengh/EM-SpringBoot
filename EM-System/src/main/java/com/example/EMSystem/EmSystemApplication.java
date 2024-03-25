a703ddc18731f55da1ef86b6f1f2886fda41bff9
sachinsingh <sachinseengh@gmail.com>
2024-03-25 09:42:48 +0545

    EM-SpringBoot

---
+++ EM-System/src/main/java/com/example/EMSystem/EmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.EMSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmSystemApplication.class, args);
+	}
+
+}
